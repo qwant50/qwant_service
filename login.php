71ac2fadf99f18444f92ff5fc6e6cf7009de5c2b
Qwant50 <qwantonline@gmail.com>
2015-10-24 16:47:31 +0300

    Login & logout pages

--- login.php
+++ login.php
@@ -42,6 +42,7 @@ if ((isset($_POST['username'])) && (isset($_POST['password']))) {
 
     <div class="row">
         <div class=" col-md-4 col-md-offset-4">
+
             <div class="panel panel-default">
                 <div class="panel-heading"><strong class="">Login</strong>
                 </div>
